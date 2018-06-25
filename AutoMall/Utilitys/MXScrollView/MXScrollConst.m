f6526efce07b6b8531776b4348b5633fd1a1296d
lijunyang <135320562@qq.com>
2018-06-25 17:06:37 +0800

    广告滚动时间改为5秒，修复日期选择下移20像素的问题

--- AutoMall/Utilitys/MXScrollView/MXScrollConst.m
+++ AutoMall/Utilitys/MXScrollView/MXScrollConst.m
@@ -9,7 +9,7 @@ CGFloat const kMXScrollViewHeight = 200; //默认滚动视图高度
 CGFloat const kMXPageControlHeight = 20; //pageControl高度
 CGFloat const kMXNavigationBarHeight = 64;//导航栏高度
 
-float const kMXScrollDuringTime = 3.0f; //默认滚动时间
+float const kMXScrollDuringTime = 5.0f; //默认滚动时间
 float const kMXAnimotionDuringTime = .5f; //默认动画时间
 
 int const kAnimotionTypeCounts = 10;//动画类型数量
